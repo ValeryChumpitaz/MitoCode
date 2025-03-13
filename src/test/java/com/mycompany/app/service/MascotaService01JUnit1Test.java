d5a66541367e5761c213ddf438388f12eda1d94e
ValeryChumpitaz <valery.chumpitaz@vallegrande.edu.pe>
2025-03-13 14:09:28 -0500

    test

---
+++ src/test/java/com/mycompany/app/service/MascotaService01JUnit1Test.java
@@ -0,0 +1,15 @@
+package com.mycompany.app.service;
+
+import com.mycompany.app.model.Mascota;
+import com.mycompany.app.model.Propietario;
+import com.mycompany.app.repository.MascotaRepository;
+import com.mycompany.app.repository.MascotaRepositoryImpl;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MascotaService01JUnit1Test {
+
+
+}
